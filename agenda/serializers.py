2cbf576275552b3eca8e012fe9deea368dc9a073
Thiago Nobre <thiagocardoso.nobre@gmail.com>
2025-07-28 00:36:31 -0400

    Criação do método DELETE + refatoração

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 
+from agenda.models import Agendamento
+
 
 class AgendamentoSerializer(serializers.Serializer):
    data_horario = serializers.DateTimeField()
    nome_cliente = serializers.CharField(max_length=200)
    email_cliente = serializers.EmailField()
    telefone_cliente = serializers.CharField(max_length=20)
+   
+   def create(self, validated_data):
+      agendamento = Agendamento.instanceects.create(
+            data_horario=validated_data["data_horario"],
+            nome_cliente=validated_data["nome_cliente"],
+            email_cliente=validated_data["email_cliente"],
+            telefone_cliente=validated_data["telefone_cliente"],
+         )
+      return agendamento
+   
+   def update(self, instance, validated_data):
+      instance.data_horario = validated_data.get("data_horario", instance.data_horario)
+      instance.nome_cliente = validated_data.get("nome_cliente", instance.nome_cliente)
+      instance.email_cliente = validated_data.get("email_cliente", instance.email_cliente)
+      instance.telefone_cliente = validated_data.get("telefone_cliente", instance.telefone_cliente)
+      instance.save()
+      return instance
--- agenda/views.py
+++ agenda/views.py
@@ -1,34 +1,55 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from agenda.serializers import AgendamentoSerializer
 from agenda.models import Agendamento
 
 # Create your views here.
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def agendamento_detail(request, id):
    obj = get_object_or_404(Agendamento, id=id)
-   serializer = AgendamentoSerializer(obj)
-   return JsonResponse(serializer.data)
-
+   if request.method == "GET":
+      serializer = AgendamentoSerializer(obj)
+      return JsonResponse(serializer.data)
+   if request.method == "PATCH":
+      data = request.data
+      serializer = AgendamentoSerializer(obj, data=data, partial=True)
+      if serializer.is_valid():
+         serializer.save()
+         return JsonResponse(serializer.data, status=200)
+      return JsonResponse(serializer.errors, status=400)
+   if request.method == "DELETE":
+      obj.cancelado = True
+      obj.save()
+      return Response(status=204)
+      
 
 @api_view(http_method_names=["GET", "POST"])
 def agendamento_list(request):
    if request.method == "GET":
-      qs = Agendamento.objects.all()
+      cancelado = request.GET.get("cancelado")  # ?cancelado=true ou false
+      if cancelado is not None:
+         # Converte o valor da query string para booleano
+         if cancelado.lower() == "true":
+            qs = Agendamento.objects.filter(cancelado=True)
+         elif cancelado.lower() == "false":
+            qs = Agendamento.objects.filter(cancelado=False)
+         else:
+            # Valor inválido (nem "true" nem "false")
+            return JsonResponse({"erro": "Valor inválido para 'cancelado'. Use true ou false."}, status=400)
+      else:
+         # Se não passar nenhum parâmetro, retorna todos
+         qs = Agendamento.objects.all()
+
       serializer = AgendamentoSerializer(qs, many=True)
       return JsonResponse(serializer.data, safe=False)
+
    if request.method == "POST":
       data = request.data
       serializer = AgendamentoSerializer(data=data)
       if serializer.is_valid():
-         validated_data = serializer.validated_data
-         Agendamento.objects.create(
-            data_horario=validated_data["data_horario"],
-            nome_cliente=validated_data["nome_cliente"],
-            email_cliente=validated_data["email_cliente"],
-            telefone_cliente=validated_data["telefone_cliente"],
-         )
+         serializer.save()
          return JsonResponse(serializer.data, status=201)
-      return JsonResponse(serializer.errors, status=400,)
+      return JsonResponse(serializer.errors, status=400)
