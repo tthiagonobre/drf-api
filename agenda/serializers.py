acf312429126a14920d0d45350e036ea6ac37b71
Thiago Nobre <thiagocardoso.nobre@gmail.com>
2025-07-29 19:22:24 -0400

    Refatoração e testes

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,27 +1,25 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from agenda.models import Agendamento
 
 
-class AgendamentoSerializer(serializers.Serializer):
-   data_horario = serializers.DateTimeField()
-   nome_cliente = serializers.CharField(max_length=200)
-   email_cliente = serializers.EmailField()
-   telefone_cliente = serializers.CharField(max_length=20)
+class AgendamentoSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Agendamento
+      fields = ["id", "data_horario", "nome_cliente", "email_cliente", "telefone_cliente", "cancelado"]
    
-   def create(self, validated_data):
-      agendamento = Agendamento.instanceects.create(
-            data_horario=validated_data["data_horario"],
-            nome_cliente=validated_data["nome_cliente"],
-            email_cliente=validated_data["email_cliente"],
-            telefone_cliente=validated_data["telefone_cliente"],
-         )
-      return agendamento
+   def validate_data_horario(self, value):
+      if value < timezone.now():
+         raise serializers.ValidationError("Agendamento não pode ser feito no passado!")
+      return value
    
-   def update(self, instance, validated_data):
-      instance.data_horario = validated_data.get("data_horario", instance.data_horario)
-      instance.nome_cliente = validated_data.get("nome_cliente", instance.nome_cliente)
-      instance.email_cliente = validated_data.get("email_cliente", instance.email_cliente)
-      instance.telefone_cliente = validated_data.get("telefone_cliente", instance.telefone_cliente)
-      instance.save()
-      return instance
+   
+   def validate(self, attrs):
+      telefone_cliente = attrs.get("telefone_cliente", "")
+      email_cliente = attrs.get("email_cliente", "")
+      
+      if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_cliente.startswith("+55"):
+         raise serializers.ValidationError("Email brasileiro deve estar associado a telefone do Brasil (+55)")
+      return attrs
+   
--- agenda/tests.py
+++ agenda/tests.py
@@ -1,3 +1,66 @@
-from django.test import TestCase
+from django.utils.timezone import make_aware
+from datetime import datetime
+import json
+from rest_framework.test import APITestCase
+
+from agenda.models import Agendamento
 
 # Create your tests here.
+class TestListagemAgendamentos(APITestCase):
+   def test_listagem_vazia(self):
+      response = self.client.get("/api/agendamentos/")
+      data = json.loads(response.content)
+      self.assertEqual(data, [])
+      
+      
+   def test_listagem_de_agendamentos_criados(self):      
+      Agendamento.objects.create(
+         data_horario = datetime(2025, 8, 2),
+         nome_cliente = "Thiago",
+         email_cliente = "tcardosonobre@gmail.com",
+         telefone_cliente = "92988339327",
+         cancelado = False,
+      )
+      
+      agendamento_serialiazado = {
+         "id": 1,
+         "data_horario": "2025-08-02T00:00:00Z",
+         "nome_cliente": "Thiago",
+         "email_cliente": "tcardosonobre@gmail.com",
+         "telefone_cliente": "92988339327",
+         "cancelado": False,
+      }
+      
+      response = self.client.get("/api/agendamentos/")
+      data = json.loads(response.content)
+      self.assertDictEqual(data[0], agendamento_serialiazado)
+      
+      
+class TestCriacaoAgendamento(APITestCase):
+   def test_cria_agendamento(self):
+      agendamento_request_data = {
+         "data_horario": "2025-08-02T00:00:00Z",
+         "nome_cliente": "Thiago",
+         "email_cliente": "tcardosonobre@gmail.com",
+         "telefone_cliente": "92988339327",
+         "cancelado": False,
+      }
+      
+      response = self.client.post("/api/agendamentos/", agendamento_request_data, format="json")
+      self.assertEqual(response.status_code, 201)
+      
+      response_get = self.client.get("/api/agendamentos/")
+      self.assertEqual(response_get.status_code, 200)
+      
+      data = json.loads(response_get.content)
+      
+      agendamento_serialiazado = {
+         "id": 1,
+         "data_horario": "2025-08-02T00:00:00Z",
+         "nome_cliente": "Thiago",
+         "email_cliente": "tcardosonobre@gmail.com",
+         "telefone_cliente": "92988339327",
+         "cancelado": False,
+      }
+      
+      self.assertEqual(data[0], agendamento_serialiazado)
